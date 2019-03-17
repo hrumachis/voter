119ff883aef7ea81c0b96f1e8a76f51769a9825e
Hrumachis <eu.andrius.mikalauskas@gmail.com>
2019-03-17 18:33:38 +0200

    v0.0.5
    Server configurations

--- server/_dist/models/Const.js
+++ server/_dist/models/Const.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class CONST {
 }
 CONST.DISTNAME = 'dist';
-CONST.__DISTNAME = `${__dirname.split("server")[0]}dist`;
+CONST.__DISTNAME = `${__dirname.split("server")[0]}dist/voter`;
 CONST.__DIRNAME = `${__dirname.split("server")[0].slice(0, -1)}`;
 exports.default = CONST;
--- server/models/Const.ts
+++ server/models/Const.ts
@@ -1,5 +1,5 @@
 export default class CONST {
     public static DISTNAME = 'dist';
-    public static __DISTNAME = `${ __dirname.split("server")[0] }dist`;
+    public static __DISTNAME = `${ __dirname.split("server")[0] }dist/voter`;
     public static __DIRNAME = `${ __dirname.split("server")[0].slice(0, -1) }`;
 }
